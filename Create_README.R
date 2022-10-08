28cd3502b54f43d38eb61699c086281ab90ad53a
Luke Perkins <lrperki2@ncsu.edu>
2022-10-08 11:59:59 -0400

    Remove toc float and code folding

--- Create_README.R
+++ Create_README.R
@@ -3,9 +3,7 @@ rmarkdown::render("Project_2_Vignette.Rmd",
                   output_format = "github_document",
                   output_options = list(
                     toc = TRUE,
-                    toc_float = TRUE,
                     number_sections = TRUE,
-                    code_folding = "show",
                     df_print = "tibble"
                   )
 )
