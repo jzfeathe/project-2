ce5735e9c57b5f2dd72d84e5d237fc61daa1b131
Justin Feathers <jzfeathe@ncsu.edu>
2022-10-12 19:09:46 -0400

    Almost there

--- Create_README.R
+++ Create_README.R
@@ -1,5 +1,7 @@
-rmarkdown::render(input = "Project_2_Vignette.Rmd",
-                  output_file = "README.md"
+rmarkdown::render(input = "Project_2_Vignette.Rmd", output_file = "README.md")
 
-)
+
+
+
+
 
