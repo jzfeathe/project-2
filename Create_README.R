2fa599cbc2e30d452561c67304fa3e2cc620b8ae
Justin Feathers <jzfeathe@ncsu.edu>
2022-10-12 18:44:28 -0400

    another test...

--- Create_README.R
+++ Create_README.R
@@ -1,7 +1,9 @@
 rmarkdown::render("Project_2_Vignette.Rmd",
                   output_file = "README.md",
                   output_format = "github_document",
+                  output_options = list(
+                    toc = TRUE,
+                    number_sections = TRUE,
+                    df_print = "tibble"
                   )
-
-
-                  
+)
